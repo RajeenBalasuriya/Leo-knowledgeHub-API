10b0ca91cdfe7ff299e896c7124d20f02fe16efc
Rajeen Balasuriya <102136700+RajeenBalasuriya@users.noreply.github.com>
2025-01-20 00:44:04 +0530

    completed JWT token generation without password hashing

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,15 +1,29 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards,Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { signInDto } from './dtos/signIn.dto';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/login')
-   signIn(@Body() signInDtoObj:signInDto ){
-
-    return this.authService.signIn(signInDtoObj.email,signInDtoObj.password);
-    
+  signIn(@Body() signInDtoObj: signInDto) {
+    return this.authService.signIn(signInDtoObj.email, signInDtoObj.password);
   }
+
+  //end point to check the auth guard
+  @UseGuards(AuthGuard)
+  @Get('/profile')
+  getProfie(@Request() req){
+    return req.user;
+  }
+
+   //end point to check the auth guard
+
+   @Get('/profiles')
+   getProfies(@Request() req){
+     return "Hi";
+   }
+
 }
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from 'src/config/config.service';
 
 @Module({
-  imports:[UserModule],
+  imports: [
+    UserModule,
+    JwtModule.registerAsync({
+      imports: [], // Add other imports if needed
+      inject: [ConfigService], // Inject ConfigService
+      useFactory: async (configService: ConfigService) => ({
+        global:true,
+        secret: configService.getJwtSecret(), // Get the JWT secret from ConfigService
+        signOptions: { expiresIn: '60s' },   // Add other sign options if needed
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
+  exports: [AuthService], // Export AuthService if needed by other modules
 })
 export class AuthModule {}
